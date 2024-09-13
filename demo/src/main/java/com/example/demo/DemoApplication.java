1673ee3c8291ecb897684f48a51d68a0d0300137
MikhailKapitov <mkapitov4@gmail.com>
2024-09-13 12:58:24 +0500

    Foreign commit! Added one more comment.

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //Misha mne pomog 
+// Ha-ha, eto ya.
 
 @SpringBootApplication
 public class DemoApplication {
