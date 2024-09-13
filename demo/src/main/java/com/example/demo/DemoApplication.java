0f916a62f5de50fc734ef9f898bca01d703c19c3
MikhailKapitov <mkapitov4@gmail.com>
2024-09-13 08:24:38 +0500

    Added a useless comment to the DemoApplication.java.

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
+        // Just a comment to make a commit.
+        
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
