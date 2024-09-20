b28ba0f7b766f6c9779963c0bb1b4ce5b9d41c20
nodirbekUmarov04 <nadir.ymarov@gmail.com>
2024-09-20 09:37:21 +0500

    just comment

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //Misha mne pomog 
 // Ha-ha, eto ya.
+//just second comment 
 
 @SpringBootApplication
 public class DemoApplication {
