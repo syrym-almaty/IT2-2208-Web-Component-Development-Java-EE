8ac1ae3e2985f6c65f8a6330f0b09e5c01e7c9f6
nodirbekUmarov04 <nadir.ymarov@gmail.com>
2024-09-27 09:04:34 +0500

    I deleted comment

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //Misha mne pomog 
 // Ha-ha, eto ya.
-//just second comment 
+//uousdgpfesd
 
 @SpringBootApplication
 public class DemoApplication {
