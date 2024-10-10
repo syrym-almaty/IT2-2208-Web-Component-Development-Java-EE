8ef30b44cab6f833a2fe7a5386c9393122b1f787
Andy <kanatnuris0@gmail.com>
2024-10-10 20:46:06 +0500

    First Commit

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
+
+		System.out.println("Good job!");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
