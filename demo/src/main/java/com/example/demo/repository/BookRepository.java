462d6878e12b3f5167ebb9da770d5e9aa90434f7
Bbbakhtiyar <bahtiarchuzebaev@gmail.com>
2024-10-23 23:20:21 +0500

    little practice 5

--- demo/src/main/java/com/example/demo/repository/BookRepository.java
+++ demo/src/main/java/com/example/demo/repository/BookRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+    ScopedValue<Object> findById(UUID id);
+
+    void deleteById(UUID id);
 }
