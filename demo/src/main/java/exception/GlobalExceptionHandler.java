1a53477acb67c49da43d5b17e865a22e97009828
sayka08 <s.serik@ovtech.kz>
2024-10-24 13:51:05 +0600

    little_lab1_sayatbek

--- demo/src/main/java/exception/GlobalExceptionHandler.java
+++ demo/src/main/java/exception/GlobalExceptionHandler.java
@@ -2,10 +2,13 @@ package exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -25,6 +28,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BusinessException.class)
     public <BusinessException> ResponseEntity<String> handleBusinessException(BusinessException ex) {
         return ResponseEntity.badRequest().body(ex.getClass());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
