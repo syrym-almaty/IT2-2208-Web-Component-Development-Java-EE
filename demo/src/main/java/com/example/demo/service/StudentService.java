9c6331db6efbf2b8197414cfe5cd8c91495d5e3c
sayka08 <s.serik@ovtech.kz>
2024-10-24 13:32:25 +0600

    labus1_Sayatbek

--- demo/src/main/java/com/example/demo/service/StudentService.java
+++ demo/src/main/java/com/example/demo/service/StudentService.java
@@ -1,7 +1,10 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Grade;
 import com.example.demo.entity.Student;
+import com.example.demo.repository.GradeRepository;
 import com.example.demo.repository.StudentRepository;
+import exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,9 @@ public class StudentService {
 
     @Autowired
     private StudentRepository studentRepository;
+
+    @Autowired
+    private GradeRepository gradeRepository;
 
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
@@ -29,4 +35,28 @@ public class StudentService {
     public void deleteStudent(UUID id) {
         studentRepository.deleteById(id);
     }
+
+    public Double calculateGPA(UUID studentId) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new ResourceNotFoundException("Student not found with id " + studentId));
+
+        List<Grade> grades = gradeRepository.findByStudent(student);
+
+        if(grades.isEmpty()){
+            return 0.0;
+        }
+
+        double totalScore = grades.stream()
+                .mapToDouble(Grade::getScore)
+                .sum();
+
+        double gpa = totalScore / grades.size();
+
+        student.setGpa(gpa);
+        studentRepository.save(student)
+
+        return gpa;
+    }
+
+
 }
--- demo/src/main/java/exception/GlobalExceptionHandler.java
+++ demo/src/main/java/exception/GlobalExceptionHandler.java
@@ -22,6 +22,16 @@ public class GlobalExceptionHandler {
 
     // You can add more exception handlers here
 
+    @ExceptionHandler(BusinessException.class)
+    public <BusinessException> ResponseEntity<String> handleBusinessException(BusinessException ex) {
+        return ResponseEntity.badRequest().body(ex.getClass());
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
     // Inner class for error response
     public static class ErrorResponse {
         private int status;
